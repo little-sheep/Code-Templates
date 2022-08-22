e5e31687d092674c4d2a5d397f8bd6b2a59b1145
little-sheep <1223125595@qq.com>
2022-08-22 13:21:10 +0800

    add a dropdown button

---
+++ buttons/dropdown.js
@@ -0,0 +1,7 @@
+const dropdownBtn = document.getElementById("dropdown-btn");
+
+const dropdownContent = document.getElementById("dropdown-content");
+
+dropdownBtn.onclick = function () {
+  dropdownContent.classList.toggle("show");
+};
