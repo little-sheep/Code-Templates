0d4510e6a16ee3dd3a13db91cc2705b975320c26
little-sheep <1223125595@qq.com>
2022-08-24 14:19:09 +0800

    add a calculate button and a thumbsup button

---
+++ buttons/calculate.js
@@ -0,0 +1,100 @@
+const calculateBtn = document.getElementById("calculate-btn");
+const calculateNum = document.getElementById("calculate-number");
+const thumbsupBtn = document.getElementById("thumbsup-btn");
+const thumbsupNum = document.getElementById("thumbsup-number");
+
+// localStorage.clear();
+
+/*
+ * Calculate Button
+ */
+
+// localStorage.setItem("calculateSum", "0");
+
+changeCalculateColor();
+calculateNum.innerText = localStorage.calculateSum;
+calculateBtn.addEventListener("click", calculate);
+
+function calculate() {
+  if (localStorage.calculateSum) {
+    localStorage.calculateSum = Number(localStorage.calculateSum) + 1;
+  } else {
+    localStorage.calculateSum = 1;
+  }
+  changeCalculateColor();
+  calculateNum.innerText = localStorage.calculateSum;
+  // thumbsupNum.innerText = localStorage.calculateSum;
+}
+
+/*
+ * Thumbsup Button
+ */
+
+changeThumbsColor();
+// localStorage.setItem("thumbsupSum", "0");
+
+thumbsupNum.innerText = localStorage.thumbsupSum;
+// thumbsupNum.innerText = localStorage.calculateSum;
+
+thumbsupBtn.addEventListener("click", changeSomething);
+
+function changeSomething() {
+  if (localStorage.thumbsupSum > 0) {
+    if (localStorage.calculateSum > 0) {
+      localStorage.calculateSum--;
+    }
+    localStorage.thumbsupSum = 0;
+    // console.log(`uncheck`);
+  } else {
+    localStorage.calculateSum++;
+    localStorage.thumbsupSum = 1;
+    // console.log(`checked`);
+  }
+  changeThumbsColor();
+  // thumbsupNum.innerText = localStorage.calculateSum;
+  thumbsupNum.innerText = localStorage.thumbsupSum;
+  calculateNum.innerText = localStorage.calculateSum;
+}
+
+// Change Color
+
+// function changeCalculateColor() {
+//   if (localStorage.calculateSum > 0) {
+//     calculateBtn.classList.remove("calculate-0");
+//     calculateBtn.classList.add("calculate-1");
+//   } else {
+//     localStorage.thumbsupSum = 0;
+//     calculateBtn.classList.remove("calculate-1");
+//     calculateBtn.classList.add("calculate-0");
+//   }
+// }
+
+// better
+function changeCalculateColor() {
+  if (localStorage.calculateSum > 0) {
+    if (calculateBtn.classList.contains("calculate-0")) {
+      calculateBtn.classList.remove("calculate-0");
+      // console.log(`${calculateBtn.classList}`);
+    }
+    if (!calculateBtn.classList.contains("calculate-1")) {
+      calculateBtn.classList.add("calculate-1");
+      // console.log(`${calculateBtn.classList}`);
+    }
+    // console.log("> 0");
+  } else {
+    localStorage.thumbsupSum = 0;
+    // console.log("= 0");
+  }
+}
+
+function changeThumbsColor() {
+  if (localStorage.thumbsupSum > 0) {
+    // thumbsupBtn.style.color = "#535c49";
+    thumbsupBtn.classList.remove("thumbsup-0");
+    thumbsupBtn.classList.add("thumbsup-1");
+  } else {
+    // thumbsupBtn.style.color = "#a5b892";
+    thumbsupBtn.classList.remove("thumbsup-1");
+    thumbsupBtn.classList.add("thumbsup-0");
+  }
+}
