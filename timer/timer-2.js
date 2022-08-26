9e5e5dc8511a5f24801fc4183738b7dcfe64eeed
little-sheep <1223125595@qq.com>
2022-08-26 13:28:36 +0800

    bug of timer-2 fixed

--- timer/timer-2.js
+++ timer/timer-2.js
@@ -19,8 +19,24 @@ timer2ClearBtn.addEventListener("click", clearTime);
 // console.log(typeof time.hour);
 
 function startTime() {
-  updateTime = setInterval(updateTimeValue, 1000, time);
-  updateDisplay = setInterval(updateTimer, 0);
+  timer2ClearBtn.classList.add("timer-2-clear-uncheck");
+  timer2ClearBtn.classList.remove("checked");
+
+  timer2StopBtn.classList.remove("checked");
+  timer2StopBtn.classList.add("timer-2-stop-uncheck");
+
+  timer2StartBtn.classList.remove("timer-2-start-uncheck");
+  timer2StartBtn.classList.add("checked");
+
+  if (!updateTime) {
+    // console.log(`updateTime: ${updateTime}`);
+    updateTime = setInterval(updateTimeValue, 1000, time);
+  } else {
+  }
+  if (!updateDisplay) {
+    // console.log(`updateDisplay: ${updateDisplay}`);
+    updateDisplay = setInterval(updateTimer, 1001);
+  }
 }
 
 function updateTimeValue(obj) {
@@ -38,12 +54,28 @@ function updateTimeValue(obj) {
 }
 
 function stopTime() {
+  timer2StopBtn.classList.remove("timer-2-stop-uncheck");
+  timer2StopBtn.classList.add("checked");
+  timer2StartBtn.classList.remove("checked");
+  timer2StartBtn.classList.add("timer-2-start-uncheck");
+
   clearInterval(updateTime);
   clearInterval(updateDisplay);
+  updateTime = undefined;
+  updateDisplay = undefined;
   updateTimer();
 }
 
 function clearTime() {
+  timer2StopBtn.classList.remove("checked");
+  timer2StopBtn.classList.add("timer-2-stop-uncheck");
+
+  timer2StartBtn.classList.remove("checked");
+  timer2StartBtn.classList.add("timer-2-start-uncheck");
+
+  timer2ClearBtn.classList.remove("timer-2-clear-uncheck");
+  timer2ClearBtn.classList.add("checked");
+
   stopTime();
   time.hour = 0;
   time.minute = 0;
